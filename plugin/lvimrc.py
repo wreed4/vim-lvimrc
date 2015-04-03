cc2b84cbb9892afff252373738622ff6db915e0a
wreed4 <williebreed@gmail.com>
2015-04-03 19:12:03 -0400

    Added a command to print all files loaded by this plugin. Updated the documentation accordingly

--- plugin/lvimrc.py
+++ plugin/lvimrc.py
@@ -6,9 +6,16 @@ homedir = os.path.expandvars("$HOME")
 dirnames = [name for name in cwd.split('/') if name not in homedir.split('/')]
 
 curpath = homedir.split('/')
+all_files = []
 for level in dirnames[:]: # copy dirnames because we're going to be modifying it in this loop 
     curpath.append(dirnames.pop(0))
     lvimrc =  '/{}/.lvimrc'.format('/'.join(curpath))
     if os.path.exists(lvimrc):
         vim.command('source ' + lvimrc)
+        all_files.append(lvimrc)
     
+
+
+def print_files():
+    for f in all_files:
+        print f
--- plugin/lvimrc.vim
+++ plugin/lvimrc.vim
@@ -1,1 +1,4 @@
 pyfile <sfile>:p:h/lvimrc.py
+
+" Print all files loaded by this plugin
+command Lvimrc python print_files()
