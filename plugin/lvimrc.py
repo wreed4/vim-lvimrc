b44341b027ed95b497242ae2db13edbd211fa49d
wreed4 <williebreed@gmail.com>
2015-04-03 18:42:05 -0400

    incremental changes

--- plugin/lvimrc.py
+++ plugin/lvimrc.py
@@ -1,4 +1,14 @@
 import vim, os.path
 
-curdir = os.getcwd()
-print curdir
+cwd = os.getcwd()
+homedir = os.path.expandvars("$HOME")
+
+dirnames = [name for name in cwd.split('/') if name not in homedir.split('/')]
+
+for level in dirnames[:]: # copy dirnames because we're going to be modifying it in this loop 
+    curdir = homedir + '/' + '/'.join(dirnames)
+    lvimrc =  curdir + "/.lvimrc"
+    if os.path.exists(lvimrc):
+        vim.command('source lvimrc')
+    
+    dirnames.pop()
